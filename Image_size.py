bb24bcdc35b98c6addb481a756db91ed887c011e
Utkarsh Saxena <saxenautkarshmvm555@gmail.com>
2025-04-01 17:37:07 +0530

    Added Analytical codes

---
+++ Image_size.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+from collections import Counter
+
+train_path = "/home/utkarsh/Desktop/Sem-2/Deep Learning/2024PGCSDS14_Utkarsh Saxena_DeepLearning/coco_dataset/train2017/"
+image_files = [os.path.join(train_path, f) for f in os.listdir(train_path) if f.endswith(('.jpg', '.png', '.jpeg'))]
+image_sizes = []
+
+for img_path in image_files:
+    image = cv2.imread(img_path)
+    if image is not None:
+        h, w, _ = image.shape  
+        image_sizes.append((w, h))
+
+size_counts = Counter(image_sizes)
+sizes = [f"{w}x{h}" for w, h in size_counts.keys()]
+counts = list(size_counts.values())
+plt.figure(figsize=(12, 12))
+plt.bar(sizes, counts, color="skyblue")
+plt.xlabel("Image Size (Width x Height)")
+plt.ylabel("Frequency")
+plt.title("Distribution of Image Sizes in Dataset (Direct from Images)")
+plt.xticks(rotation=90)  
+plt.show()
--- Visualization_V1.py
+++ Visualization_V1.py
@@ -6,22 +6,17 @@ import os
 json_path = "/home/utkarsh/Desktop/Sem-2/Deep Learning/2024PGCSDS14_Utkarsh Saxena_DeepLearning/coco_dataset/annotations/instances_train2017.json"
 train_path = '/home/utkarsh/Downloads/train2017/'
 
-# Load COCO annotations
 with open(json_path, 'r') as f:
     coco_data = json.load(f)
 
 annotations = coco_data["annotations"]
 images = coco_data["images"]
-
-# Create a mapping from image_id to filename
 image_id_to_filename = {img["id"]: img["file_name"] for img in images}
-
-# Create a mapping from image filename to bounding boxes
 image_to_bboxes = {}
 
 for ann in annotations:
     image_id = ann["image_id"]
-    bbox = ann["bbox"]  # [x, y, width, height]
+    bbox = ann["bbox"] 
     
     filename = image_id_to_filename.get(image_id)
     if filename:
@@ -30,7 +25,6 @@ for ann in annotations:
             image_to_bboxes[image_path] = []
         image_to_bboxes[image_path].append(bbox)
 
-# Loop through images and draw bounding boxes
 for image_path, bboxes in image_to_bboxes.items():
     if not os.path.exists(image_path):
         print(f"Image not found: {image_path}")

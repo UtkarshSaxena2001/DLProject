155bc1fa7b38816a0438fe9686cabd3830c942d2
Rakshit Ranjan <ranjanrakshit2001@gmail.com>
2025-04-02 18:28:33 +0530

    train json creation

--- Train_json_editor.py
+++ Train_json_editor.py
@@ -48,5 +48,5 @@ for i in tqdm.tqdm(data['images']):
     if img["bbox"] != 0:
         l.append(img)
 
-with open("Max_Area_BB_validation.json",'w+') as file:
+with open("data/Max_Area_BB_train.json",'w+') as file:
     json.dump(l, file, indent=4)
